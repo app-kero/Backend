c50a1687ed5596149d6640f93dd4d76bd48202eb
luccasocastro <emaildocastro.adm01@gmail.com>
2024-09-21 16:52:06 -0300

    feat: prevenindo codigo repetitivo

--- src/main/java/com/appkero/backend_kero/entities/Usuario.java
+++ src/main/java/com/appkero/backend_kero/entities/Usuario.java
@@ -1,9 +1,6 @@
 package com.appkero.backend_kero.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,10 +13,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class Usuario {
-    
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Usuario extends BasicEntity {
 
     private String nome;
     private String sobrenome;
